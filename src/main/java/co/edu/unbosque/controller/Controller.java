21f9b6a4d59637ba28051d63b6c9d5db6d6c7b2b
kevinpinzon02 <kevinpinzon02@gmail.com>
2022-09-28 08:50:58 -0500

    Final version with a option of search

--- src/main/java/co/edu/unbosque/controller/Controller.java
+++ src/main/java/co/edu/unbosque/controller/Controller.java
@@ -2,6 +2,7 @@
  * The package of the class
  */
 package co.edu.unbosque.controller;
+
 /**
  * The necessary imports to work the class
  */
@@ -17,7 +18,7 @@ import co.edu.unbosque.view.View;
  * @author Omar Santos, Hernan Alvarado, Kevin Pinzón
  *
  */
-public class Controller implements ActionListener{
+public class Controller implements ActionListener {
 
 	/**
 	 * Variable of the class Algorithm
@@ -31,9 +32,10 @@ public class Controller implements ActionListener{
 	 * Variable of the class View
 	 */
 	private View view;
-	
+
 	/**
-	 * Constructor of the class, it calls a Listeners method and instances the classes declared before
+	 * Constructor of the class, it calls a Listeners method and instances the
+	 * classes declared before
 	 */
 	public Controller() {
 		model = new Algorithm();
@@ -41,7 +43,7 @@ public class Controller implements ActionListener{
 		view = new View();
 		assigningListeners();
 	}
-	
+
 	/**
 	 * Method that assigns the buttons listeners of the view
 	 */
@@ -49,16 +51,17 @@ public class Controller implements ActionListener{
 		view.getPanelMain().getBtnImportFile().addActionListener(this);
 		view.getPanelMain().getBtnSearch().addActionListener(this);
 		view.getPanelMain().getRadioBM().addActionListener(this);
-		view.getPanelMain().getRadioKMB().addActionListener(this);	
+		view.getPanelMain().getRadioKMB().addActionListener(this);
 	}
 
 	/**
-	 * Method that connects the differents options of the view with the model. Also makes validations
+	 * Method that connects the differents options of the view with the model. Also
+	 * makes validations
 	 */
 	public void actionPerformed(ActionEvent e) {
 		String comand = e.getActionCommand();
 		String text = null;
-		if(comand.equals("IMPORTFILE")) {
+		if (comand.equals("IMPORTFILE")) {
 			text = persistence.contentText(view.connectFileChooser().getAbsolutePath());
 			view.getPanelMain().getShowText().setText(text);
 			view.getPanelMain().getText().setVisible(true);
@@ -70,41 +73,63 @@ public class Controller implements ActionListener{
 			view.getPanelMain().getRectangle().setVisible(true);
 			view.getPanelMain().getBarraSalida().setVisible(true);
 			view.getPanelMain().getOptionsSearch().setVisible(true);
-		
-		}else if(comand.equals("SEARCH")) {
+
+		} else if (comand.equals("SEARCH")) {
 			String search = view.getPanelMain().getText().getText();
 			text = persistence.contentText(view.getRuta());
-			if(!search.isEmpty()) {
+			if (!search.isEmpty()) {
 				String estados = view.getPanelMain().estados();
-				if(estados != null) {
+				String statusCombo = view.getPanelMain().getOptionsSearch().getSelectedItem().toString();
+				if (estados != null) {
+					
+					if(statusCombo.equals("Select a type of search")) {
+						view.warningMessage("You must select a type of search");
+					} else
 					paintText(text, estados, search);
-					
-					
-					System.out.println(view.getPanelMain().getOptionsSearch().getSelectedItem().toString());
-					
-				}else 
-					view.warningMessage("Debes seleccionar un algoritmo");
-			}else {
-				view.warningMessage("Debes ingresar un texto para buscar.");
+
+				} else
+					view.warningMessage("You must select an algorithm");
+			} else {
+				view.warningMessage("You must fill out a text to perform the search");
 			}
 			view.getPanelMain().getText().setText("");
 		}
 	}
-	
+
 	/**
-	 * Method that paints the character(s) solicited by the user, considering the algorithm selected
+	 * Method that paints the character(s) solicited by the user, considering the
+	 * algorithm selected
 	 * 
-	 * @param content String where the character(s) will be searched
+	 * @param content   String where the character(s) will be searched
 	 * @param algorithm String of the algorithm selected
-	 * @param search String of character(s) that will be painted
+	 * @param search    String of character(s) that will be painted
 	 */
 	private void paintText(String content, String algorithm, String search) {
-		String chars [];
-		if(algorithm.equals("KMP")) 
+		String chars[];
+		String status = view.getPanelMain().getOptionsSearch().getSelectedItem().toString();
+		if (algorithm.equals("KMP")) {
+			if (status.equals("Capitar letter"))
+				search = search.toUpperCase();
+			if (status.equals("Lower case letter"))
+				search = search.toLowerCase();
+			if (status.equals("Without distinction")) {
+				content = content.toUpperCase();
+				search = search.toUpperCase();
+			}
 			chars = model.kmpAlgorithm(content, search).split(",");
-		else 
+
+		} else {
+			if (status.equals("Capitar letter"))
+				search = search.toUpperCase();
+			if (status.equals("Lower case letter"))
+				search = search.toLowerCase();
+			if (status.equals("Without distinction")) {
+				content = content.toUpperCase();
+				search = search.toUpperCase();
+			}
 			chars = model.bmAlgorithm(content, search).split(",");
-		view.getPanelMain().getLblCoincidences().setText("The quantity of coincidences is: " + (chars.length -1));
+		}
+		view.getPanelMain().getLblCoincidences().setText("The quantity of coincidences is: " + (chars.length - 1));
 		view.getPanelMain().getLblCoincidences().setVisible(true);
 		view.getPanelMain().paintText(chars, search.length());
 	}
